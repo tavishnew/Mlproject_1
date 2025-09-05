70c9a33294311e610ddd1753e3a45db9d73a59b8
tavishnew <tavish0554@gmail.com>
2025-09-05 20:56:23 +0530

    changes in app

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from src.components.pipeline.predict_pipe import CustomData,PredictPipeline
 
-app=Flask(__name__)
+application=Flask(__name__)
 
-app=app
+app=application
 
 ## Route for a home page
 
@@ -42,4 +42,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
+    app.run(host="0.0.0.0",debug=True)        
